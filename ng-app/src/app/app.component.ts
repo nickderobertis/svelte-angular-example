2b0c78dfba592a7a7323dc88707c77e2f3e04ccf
Nick DeRobertis <whoopnip@gmail.com>
2021-01-17 12:59:42 -0500

    Add angular app which is using svelte component

---
+++ ng-app/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import Small from '../../../svelte-app/build/dist/Small.svelte';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'ng-app';
+  svelteApp: Small;
+
+  constructor(public elRef: ElementRef, public renderer: Renderer2) {}
+
+  ngOnInit() {
+    const el: HTMLElement = this.elRef.nativeElement;
+    const newElem: HTMLDivElement = this.renderer.createElement('div');
+    newElem.id = 'svelte';
+    el.appendChild(newElem);
+    this.svelteApp = new Small({
+      target: newElem,
+    });
+  }
+}
